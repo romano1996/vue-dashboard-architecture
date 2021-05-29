e15926a4cf4fc4c21c208746c4f6d50396112bf1
Romano Schoonheim <romano@romanoschoonheim.nl>
2021-05-29 10:10:10 -0700

    Move authentication store.

--- src/store/global.js
+++ src/store/global.js
@@ -1,6 +1,6 @@
 // Register store modules.
 import { createStore } from "vuex";
-import { authentication } from "@/store/authentication";
+import { authentication } from "@/modules/authentication/store/authentication";
 import { user } from "@/modules/user/store/user";
 
 export const store = createStore({
