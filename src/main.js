b3d2428c58eb690ac21cecd825d0f16cd68a743e
Romano Schoonheim <romano@romanoschoonheim.nl>
2021-05-29 01:38:08 -0700

    Create modules folder.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import App from "./modules/authentication/App.vue";
+import router from "./modules/authentication/router";
+import store from "./modules/authentication/store";
 
 createApp(App).use(store).use(router).mount("#app");
