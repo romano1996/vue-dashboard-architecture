a93bf539640498e8796e45b24828acc167fdec17
Romano Schoonheim <romano@romanoschoonheim.nl>
2021-05-29 10:27:54 -0700

    Remove comment

--- src/store/global.js
+++ src/store/global.js
@@ -1,4 +1,3 @@
-// Register store modules.
 import { createStore } from "vuex";
 import { authentication } from "@/modules/authentication/store/authentication";
 import { user } from "@/modules/user/store/user";
