bda2c85b7d5763903cab8ee34cbda90ce712602e
Romano Schoonheim <romano@romanoschoonheim.nl>
2021-05-29 08:14:43 -0700

    Create authentication store.

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import application from "./application.vue";
 import router from "./router";
 import { createStore } from "vuex";
 import user from "@/modules/dashboard/store/user";
+import authentication from "@/store/authentication";
 
 // Register store modules.
 const store = createStore({
   modules: {
+    authentication: authentication,
     user: user,
   },
 });
