59263271e9a12e1e614b128655ff665803316832
Romano Schoonheim <romano@romanoschoonheim.nl>
2021-05-29 08:51:41 -0700

    Update authentication store.

--- src/store/global.js
+++ src/store/global.js
@@ -1,6 +1,6 @@
 // Register store modules.
 import { createStore } from "vuex";
-import authentication from "@/store/authentication";
+import { authentication } from "@/store/authentication";
 import { user } from "@/modules/dashboard/store/user";
 
 export const store = createStore({
