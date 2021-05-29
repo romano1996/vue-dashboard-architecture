fdf1711628ca7649ae3a4c793776187155193901
Romano Schoonheim <romano@romanoschoonheim.nl>
2021-05-29 12:01:36 -0700

    Install tailwind css

--- src/layouts/dashboard.vue
+++ src/layouts/dashboard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="container">
     <Navigation
       :items="[
       {to: { name: 'dashboard' }, label: 'Dashboard' },
--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from "vue";
 import application from "./application.vue";
 import router from "./routing";
 import { store } from "@/store/global";
+import "./styling/app.css";
 
 createApp(application).use(store).use(router).mount("#app");
